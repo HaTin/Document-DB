c790b25fcd85f346d3091b45aa8de96738044657
tyntin091 <tyntin091@gmail.com>
2020-04-18 17:27:43 +0700

    add query to get database columns information

---
+++ database.js
@@ -0,0 +1,22 @@
+const knex = require('./config/knex')
+const getDatabaseSchemas = async (schema) => {
+    const result = await knex('information_schema.tables'). join('information_schema.columns',function(){
+        this.on('information_schema.tables.table_schema', '=', 'information_schema.columns.table_schema')
+        .andOn('information_schema.tables.table_name','=','information_schema.columns.table_name')
+    }).
+    select('information_schema.tables.table_type','information_schema.tables.table_name',
+            'information_schema.columns.column_name', 'information_schema.columns.data_type',
+            'information_schema.columns.is_nullable')
+    .where({'information_schema.tables.table_schema': schema})
+    .groupBy('information_schema.tables.table_type','information_schema.tables.table_name',
+    'information_schema.columns.column_name', 'information_schema.columns.data_type',
+    'information_schema.columns.is_nullable').
+    orderBy('information_schema.tables.table_name')
+    return result
+}
+
+module.exports = {
+    getDatabaseSchemas
+}
+
+
