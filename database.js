53336cc75cd680a9aeccdd4951801e3b835f99a5
tyntin091 <tyntin091@gmail.com>
2020-04-19 07:59:43 +0700

    add support for mutiple databases

--- database.js
+++ database.js
@@ -1,13 +1,13 @@
-const knex = require('./config/knex')
-const getDatabaseSchemas = async (schema) => {
-    const result = await knex('information_schema.tables'). join('information_schema.columns',function(){
+const Knex = require('knex')
+const getDatabaseSchemas = async (schema, dbConfig) => {
+    const result = await Knex(dbConfig)('information_schema.tables').join('information_schema.columns',function(){
         this.on('information_schema.tables.table_schema', '=', 'information_schema.columns.table_schema')
         .andOn('information_schema.tables.table_name','=','information_schema.columns.table_name')
     }).
     select('information_schema.tables.table_type','information_schema.tables.table_name',
             'information_schema.columns.column_name', 'information_schema.columns.data_type',
             'information_schema.columns.is_nullable')
-    .where({'information_schema.tables.table_schema': schema})
+    .where({'information_schema.tables.table_schema': schema}) 
     .groupBy('information_schema.tables.table_type','information_schema.tables.table_name',
     'information_schema.columns.column_name', 'information_schema.columns.data_type',
     'information_schema.columns.is_nullable').
